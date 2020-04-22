68f574c3d0b3bb8767db625b989d926ce94a3023
Younis Latyp <younislatyp@gmail.com>
2020-04-22 12:53:31 -0400

    Adding addition

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <cmath>
+#include <hyperops.hpp>
 using namespace std;
 
 int main() {
+	long long int xlong;
+	long double xdouble;
+	long long int ylong;
+	long double ydouble;
 	string hyper;
 	cout << "Welcome to cppcalc\n";
 	cout << "We have many calcs for you";
@@ -14,6 +19,19 @@ int main() {
 		cin >> pos;
 		if (pos == "p") {
 			cout << "Pick a degree";
+			int deg;
+			deg >> cin;
+			if (deg == 0) {
+				cout << "Give me a value";
+				cin >> x;
+				cout << "The result is " << succeed(x);
+			} else if (deg == 1) {
+				cout << "Give me a value";
+				cin >> xlong;
+				cout << "Give me another value";
+				cin >> ylong;
+				cout << "The result is " << xlong + ylong;
+			}
 		}
 	}
 }
