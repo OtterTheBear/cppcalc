527d2838a07ddb865106538fcc52175b127fe5f4
Younis Latyp <younislatyp@gmail.com>
2020-04-22 17:42:26 -0400

    Trying to add a repeat function so the user can do many calcs w/o running again

--- main.cpp
+++ main.cpp
@@ -8,35 +8,40 @@ int main() {
     long double xdouble;
     long long int ylong;
     long double ydouble;
+    bool loop = true;
     string hyper;
     cout << "Welcome to cppcalc\n";
     cout << "We have many calcs for you\n";
-    cout << "Hyperoperation or no? (y/n)\n";
-    cin >> hyper;
-    if (hyper == "y") {
-        string pos;
-        cout << "Positive or negative? (p/n)\n";
-        cin >> pos;
-        if (pos == "p") {
-            cout << "Pick a degree\n";
-            int deg;
-            cin >> deg;
-            if (deg == 0) {
-                cout << "Give me a value\n";
-                cin >> xdouble;
-                cout << "The result is " << succeed(xdouble) << "\n";
-            } else if (deg == 1) {
-                cout << "Give me a value\n";
-                cin >> xdouble;
-                cout << "Give me another value\n";
-                cin >> ydouble;
-                cout << "The result is " << xdouble + ydouble << "\n";
-            } else if (deg == 2) {
-                cout << "Give me a value";
-                cin >> xdouble;
-                cout << "Give me another one";
-                cin >> ydouble;
-                cout << "The result is " << xdouble * ydouble << "\n";
+    while (loop) {
+        cout << "Hyperoperation or no? (y/n)\n";
+        cin >> hyper;
+        if (hyper == "y") {
+            string pos;
+            cout << "Positive or negative? (p/n)\n";
+            cin >> pos;
+            if (pos == "p") {
+                cout << "Pick a degree\n";
+                int deg;
+                cin >> deg;
+                if (deg == 0) {
+                    cout << "Give me a value\n";
+                    cin >> xdouble;
+                    cout << "The result is " << succeed(xdouble) << "\n";
+                    cout << "Continue?";
+                    cin >> loop;
+                } else if (deg == 1) {
+                    cout << "Give me a value\n";
+                    cin >> xdouble;
+                    cout << "Give me another value\n";
+                    cin >> ydouble;
+                    cout << "The result is " << xdouble + ydouble << "\n";
+                } else if (deg == 2) {
+                    cout << "Give me a value";
+                    cin >> xdouble;
+                    cout << "Give me another one";
+                    cin >> ydouble;
+                    cout << "The result is " << xdouble * ydouble << "\n";
+                }
             }
         }
     }
