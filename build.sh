3b1526d3133ae2d858b829e35314b1ad2de12cc2
Younis Latyp <younislatyp@gmail.com>
2020-04-23 13:39:16 -0400

    Build.sh now calls the file main instead of h

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 set -ex
 
-rm -f ../bin/hyperops.o ../bin/main.o ../bin/h
+rm -f ../bin/hyperops.o ../bin/main.o ../bin/main
 
 c++ -c -o ../bin/hyperops.o hyperops.cpp
 c++ -c -o ../bin/main.o main.cpp
 
-g++ -o ../bin/h ../bin/hyperops.o ../bin/main.o -lboost_math_c99
+g++ -o ../bin/main ../bin/hyperops.o ../bin/main.o -lboost_math_c99
 
 
